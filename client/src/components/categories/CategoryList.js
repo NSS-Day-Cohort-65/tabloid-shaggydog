ffb1bc3ff9f228b379e2d663db31c1d0d70957e5
JDOdea <jondavidodea@gmail.com>
2023-10-09 15:01:41 -0500

    implemented create category frontend

--- client/src/components/categories/CategoryList.js
+++ client/src/components/categories/CategoryList.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { deleteCategory, fetchCategories } from "../../managers/categoryManager";
-import { Button, Table } from "reactstrap";
+import { fetchCategories, deleteCategory, postCategory } from "../../managers/categoryManager";
 import { useNavigate } from "react-router-dom";
+import { Alert, Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalHeader, Table } from "reactstrap";
 
 
 export const CategoryList = () => {
@@ -9,15 +9,45 @@ export const CategoryList = () => {
 
     const navigate = useNavigate();
 
-    useEffect(() => {
+    const [newCategoryName, setNewCategoryName] = useState("");
+    const [createModal, setCreateModal] = useState(false);
+    const [createVisible, setCreateVisible] = useState(false);
+    const toggle = () => {
+        setCreateModal(!createModal);
+        if (createVisible) setCreateVisible(false);
+    };
+    const onDismiss = () => setCreateVisible(false);
+
+    const getAllCategories = () => {
         fetchCategories().then(setCategories);
-    }, []);
+    }
+
+    const handleCreate = () => {
+        if (!newCategoryName) {
+            setCreateVisible(true)
+        }
+        else {
+            const newCategory = {
+                name: newCategoryName
+            }
+
+            postCategory(newCategory)
+            .then(() => {
+                toggle();
+                getAllCategories();
+            })
+        }        
+    }
 
     const handleDelete = (id) => {
         deleteCategory(id).then(() => {
-            navigate("/categories");
+            getAllCategories();
         })
     }
+
+    useEffect(() => {
+        getAllCategories();
+    }, []);
 
     return (
         <div className="container">
@@ -48,6 +78,38 @@ export const CategoryList = () => {
                     </tbody>
                 </Table>
             </div>
+            <Button
+            onClick={toggle}>
+                Create Category
+            </Button>
+            <Modal isOpen={createModal} toggle={toggle}>
+                <div className="alert-float" style={{position: 'absolute', top: 10, left: 150}}>
+                    <Alert color="info" isOpen={createVisible} toggle={onDismiss}>
+                        Please enter a name
+                    </Alert>
+                </div>
+                <ModalHeader toggle={toggle}>Add Category</ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Label htmlFor="categoryName">Name:</Label>
+                            <Input 
+                            type="text"
+                            name="categoryName"
+                            onChange={(e) => {
+                                setNewCategoryName(e.target.value);
+                            }}
+                            />
+                        </FormGroup>
+                        <Button
+                        onClick={() => {
+                            handleCreate();
+                        }}>
+                            Submit
+                        </Button>
+                    </Form>
+                </ModalBody>
+            </Modal>
         </div>
     )
 }
