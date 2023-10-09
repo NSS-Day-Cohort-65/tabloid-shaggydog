99ec98e7605415f8265692d4b83cfe61475fa035
CalebSullivan35 <CalebSullivan357@gmail.com>
2023-10-09 15:05:08 -0500

    Post List completed

--- client/src/components/ApplicationViews.js
+++ client/src/components/ApplicationViews.js
@@ -5,58 +5,65 @@ import Register from "./auth/Register";
 import UserProfileList from "./userprofiles/UserProfilesList";
 import UserProfileDetails from "./userprofiles/UserProfileDetails";
 import { CategoryList } from "./categories/CategoryList";
+import { PostList } from "./posts/PostList";
 
 export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
-  return (
-    <Routes>
-      <Route path="/">
-        <Route
-          index
-          element={
-            <AuthorizedRoute loggedInUser={loggedInUser}>
-              <p>Welcome to Tabloid!</p>
-            </AuthorizedRoute>
-          }
-        />
-        <Route path="/userprofiles">
-          <Route
-            index
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
-                <UserProfileList />
-              </AuthorizedRoute>
-            }
-          />
-          <Route
-            path=":id"
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
-                <UserProfileDetails />
-              </AuthorizedRoute>
-            }
-          />
-        </Route>
-        <Route path="categories">
-          <Route
-            index
-            element={
-              <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
-                <CategoryList />
-              </AuthorizedRoute>
-            }
-          />
-        </Route>
+ return (
+  <Routes>
+   <Route path="/">
+    <Route
+     index
+     element={
+      <AuthorizedRoute loggedInUser={loggedInUser}>
+       <PostList />
+      </AuthorizedRoute>
+     }
+    />
+    <Route
+     path="/posts"
+     element={
+      <AuthorizedRoute loggedInUser={loggedInUser}>
+       <PostList />
+      </AuthorizedRoute>
+     }
+    />
+    <Route path="/userprofiles">
+     <Route
+      index
+      element={
+       <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
+        <UserProfileList />
+       </AuthorizedRoute>
+      }
+     />
 
-        <Route
-          path="login"
-          element={<Login setLoggedInUser={setLoggedInUser} />}
-        />
-        <Route
-          path="register"
-          element={<Register setLoggedInUser={setLoggedInUser} />}
-        />
-      </Route>
-      <Route path="*" element={<p>Whoops, nothing here...</p>} />
-    </Routes>
-  );
+     <Route
+      path=":id"
+      element={
+       <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
+        <UserProfileDetails />
+       </AuthorizedRoute>
+      }
+     />
+    </Route>
+    <Route path="categories">
+     <Route
+      index
+      element={
+       <AuthorizedRoute loggedInUser={loggedInUser} roles={["Admin"]}>
+        <CategoryList />
+       </AuthorizedRoute>
+      }
+     />
+    </Route>
+
+    <Route path="login" element={<Login setLoggedInUser={setLoggedInUser} />} />
+    <Route
+     path="register"
+     element={<Register setLoggedInUser={setLoggedInUser} />}
+    />
+   </Route>
+   <Route path="*" element={<p>Whoops, nothing here...</p>} />
+  </Routes>
+ );
 }
