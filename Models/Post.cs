a2205e7618d9a3a9f82ecb838efa45d00d9363d6
Chris Mills <chris.r.mills25@gmail.com>
2023-10-09 09:09:21 -0700

    Attributed Annotations

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tabloid.Models
 {
     public class Post
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
+        [Required]
         public string ImageLocation { get; set; }
         public DateTime CreateDateTime { get; set; }
         public DateTime PublishDateTime { get; set; }
--- Models/Reaction.cs
+++ Models/Reaction.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tabloid.Models
 {
     public class Reaction
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string ImageLocation { get; set; }
     }
 }
