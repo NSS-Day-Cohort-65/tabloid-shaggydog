6abb641e9ac187e01894df2146ed17c8eee39396
CalebSullivan35 <CalebSullivan357@gmail.com>
2023-10-13 14:17:10 -0500

    modal for picture

--- client/src/managers/userProfileManager.js
+++ client/src/managers/userProfileManager.js
@@ -1,53 +1,62 @@
 const _apiUrl = "/api/userprofile";
 
 export const getProfiles = () => {
-  return fetch(_apiUrl + "/withroles").then((res) => res.json());
+ return fetch(_apiUrl + "/withroles").then((res) => res.json());
 };
 
 export const getDeactivatedProfiles = () => {
-  return fetch(_apiUrl + "/deactivated").then((res) => res.json());
+ return fetch(_apiUrl + "/deactivated").then((res) => res.json());
 };
 
 export const getProfile = (id) => {
-  return fetch(_apiUrl + `/${id}`).then((res) => res.json());
+ return fetch(_apiUrl + `/${id}`).then((res) => res.json());
 };
 
-
 export const getProfileAndRoles = (id) => {
-  return fetch(`${_apiUrl}/withroles/${id}`).then((res) => res.json());
+ return fetch(`${_apiUrl}/withroles/${id}`).then((res) => res.json());
 };
 
 export const promoteProfile = (id) => {
-  return fetch(`${_apiUrl}/promote/${id}`).then((res)=> res.json())
-}
+ return fetch(`${_apiUrl}/promote/${id}`).then((res) => res.json());
+};
 
 export const demoteProfile = (id) => {
-  return fetch(`${_apiUrl}/demote/${id}`).then((res) => res.json());
-}
+ return fetch(`${_apiUrl}/demote/${id}`).then((res) => res.json());
+};
 
 export const editProfile = (profile) => {
-  return fetch(`${_apiUrl}/${profile.id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(profile)
-})
-}
+ return fetch(`${_apiUrl}/${profile.id}`, {
+  method: "PUT",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(profile),
+ });
+};
 
 export const deactivateProfile = (id) => {
-  return fetch(`${_apiUrl}/deactivate/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-  });
+ return fetch(`${_apiUrl}/deactivate/${id}`, {
+  method: "PUT",
+  headers: {
+   "Content-Type": "application/json",
+  },
+ });
 };
 
 export const activateProfile = (id) => {
-  return fetch(`${_apiUrl}/activate/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-  });
+ return fetch(`${_apiUrl}/activate/${id}`, {
+  method: "PUT",
+  headers: {
+   "Content-Type": "application/json",
+  },
+ });
 };
 
+export const updateProfileImage = (id, imgLocation) => {
+ console.log(JSON.stringify(imgLocation));
+ return fetch(`${_apiUrl}/updateImage/${id}`, {
+  method: "PUT",
+  headers: {
+   "Content-Type": "application/json",
+  },
+  body: JSON.stringify(imgLocation),
+ });
+};
